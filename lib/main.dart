4b67050de3195cd45c5e2a11e381530381455cf5
France! <franciscodomingosjose06798@gmail.com>
2024-06-06 15:10:30 +0100

    add provider pack

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main()async{
+  WidgetsFlutterBinding.ensureInitialized(
+    // awai
+  );
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
