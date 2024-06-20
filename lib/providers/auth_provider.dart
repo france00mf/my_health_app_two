ea6e3300621e3d22755667befc9d6e5357562674
France! <franciscodomingosjose06798@gmail.com>
2024-06-20 22:35:19 +0100

    finesh checkSign method

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,6 +1,16 @@
 
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthProvider  extends ChangeNotifier{
+  AuthProvider(){
+    checkSign();
+  }
 
+
+  void checkSign() async{
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    _isSignedIn = prefs.getBool("is_signedin") ?? false;
+    nofityListeners();
+  }
 }
