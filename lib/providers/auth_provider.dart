1936ab17e6c34087f8db54a1baf0d0e6a5db043c
France! <franciscodomingosjose06798@gmail.com>
2024-06-20 22:52:20 +0100

    cloudState pack#, labModel and other variables config on authProvider

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,8 +1,32 @@
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthProvider  extends ChangeNotifier{
+    bool _isSignedIn = false;
+  bool get isSignedIn => _isSignedIn;
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+  String? _uid;
+  String get uid => _uid!;
+  UserModel? _userModel;
+  UserModel get userModel => _userModel!;
+  List<BookingModel> _bookings = [];
+  List<TestModel> _tests = [];
+  List<LabModel> _labs = [];
+  List<TestModel> get tests => _tests;
+  List<LabModel> get labs => _labs;
+  List<BookingModel> get bookings => _bookings;
+
+  
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+  final FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
+
+
+
   AuthProvider(){
     checkSign();
   }
@@ -26,7 +50,7 @@ class AuthProvider  extends ChangeNotifier{
       await _firebaseAuth.verifyPhoneNumber(
         phoneNumber: phoneNumber,
         verificationCompleted: (PhoneAuthCredential phoneAuthCredential) {
-          
+
         }
       );
 
