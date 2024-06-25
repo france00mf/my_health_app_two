e19a72ba56b188339c12b577ccd3f56630fdbfe9
France! <franciscodomingosjose06798@gmail.com>
2024-06-25 13:28:02 +0100

    add storeFile method for save files created when saveUser

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -118,4 +118,11 @@ class AuthProvider  extends ChangeNotifier{
         notifyListeners();
     }
   }
+
+  Future<String> storeFileToStorage(String ref, File file)async{
+    UploadTask uploadTask = _firebaseStorage.ref().child(ref).putFile(file);
+    TaskSnapshot snapshot = await uploadTask;
+    String downloadUrl = await snapshot.ref.getDownloadURL();
+    return downloadUrl;
+  }
 }
