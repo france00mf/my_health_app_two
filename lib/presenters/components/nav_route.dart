55bcf0d62d040f29b5a802898710ce3fb5ade44f
France! <franciscodomingosjose06798@gmail.com>
2024-06-26 09:33:49 +0100

    finesh floatingAction Buttons

--- lib/presenters/components/nav_route.dart
+++ lib/presenters/components/nav_route.dart
@@ -1,11 +1,15 @@
 
 
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:my_health_app_two/presenters/view/cart/cart_view.dart';
 import 'package:my_health_app_two/presenters/view/main_nav/all_book/all_bookin_view.dart';
 import 'package:my_health_app_two/presenters/view/main_nav/home/home_view.dart';
 import 'package:my_health_app_two/presenters/view/main_nav/profile/profile_view.dart';
 import 'package:my_health_app_two/presenters/view/main_nav/special_offers/special_offers_view.dart';
+import 'package:my_health_app_two/providers/cart_provider.dart';
+import 'package:provider/provider.dart';
 
 class NavBar extends StatefulWidget {
   const NavBar({super.key});
@@ -27,6 +31,7 @@ class _NavBarState extends State<NavBar> {
 
   @override
   Widget build(BuildContext context) {
+    final cartProvider = Provider.of<CartProvider>(context, listen: false);
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
         elevation: 5,
@@ -48,8 +53,12 @@ class _NavBarState extends State<NavBar> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
-             Navigator.push(context, MaterialPageRoute(builder: (context) => CartView(cartProvider: CartView),));
-      })
+             Navigator.push(context, MaterialPageRoute(builder: (context) => CartView(cartProvider: cartProvider),));
+      },
+       child: Icon(Icons.shopping_cart),
+        backgroundColor: Color(0xFF3E69FE)
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
