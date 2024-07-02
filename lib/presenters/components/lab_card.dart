3050b82a36013260ae567ea203e8ceefb6909788
France! <franciscodomingosjose06798@gmail.com>
2024-07-02 14:43:19 +0100

    create labCard

---
+++ lib/presenters/components/lab_card.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+import 'package:my_health_app_two/model/lab_model.dart';
+
+class LabCard extends StatelessWidget{
+  final LabModel lab;
+
+  const LabCard({super.key, required this.lab});
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card();
+  }
+
+  
+}
